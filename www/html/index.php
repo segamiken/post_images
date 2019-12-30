e666e585f4256d9890ad4cdc564e85946bd09fa7
Ken <ken.s,vollyball@gmail.com>
2019-12-30 17:59:57 +0900

    Start creating app

---
+++ www/html/index.php
@@ -0,0 +1,21 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Post Imgage App</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="MAX_FILE_SIZE" value="">
+        <input type="file" name="image">
+        <input type="submit" value="upload">
+    </form>
+</body>
+</html>
+
